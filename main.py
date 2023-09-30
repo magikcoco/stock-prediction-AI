ad3cc7d86f67545ad9c9ea637ce4dd0d9f9e01a2
homepc <punishedtomato@gmail.com>
2023-09-30 13:29:58 -0400

    Added functions to get ticker data, comment out model making for now

--- main.py
+++ main.py
@@ -1,22 +1,68 @@
+import time
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import pandas_datareader as web
 import datetime as dt
 import yfinance as yf
+import os
+from bs4 import BeautifulSoup
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM
+from stocksymbol import StockSymbol
 
+
+def download_ticker_data(ticker_list, start_date=None, end_date=None):
+    all_data = {}
+    for ticker in ticker_list:
+        try:
+            data = yf.download(ticker, start_date=start_date, end=end_date)
+            if not data.empty:
+                all_data[ticker] = data
+            time.sleep(10)
+        except Exception as e:
+            print(f"Error downloading data for {ticker}. Error: {str(e)}")
+            time.sleep(60)
+    return all_data
+
+
+def save_data_to_csv(all_data, output_folder="."):
+    for ticker, data in all_data.items():
+        filename = os.path.join(output_folder, f"{ticker}.csv")
+        data.to_csv(filename)
+
+
+def get_ticker_list(save_to_file=True, load_from_file=True):
+    filename = "ticker_list.txt"
+    no_file = not load_from_file
+    companies = []
+    if load_from_file:
+        if os.path.exists("ticker_list.txt"):
+            with open(filename, "r") as file:
+                companies = [line.strip() for line in file]
+        else:
+            no_file = True
+    if no_file:
+        with open("api_key.txt", "r") as file:
+            api_key = file.read().strip()
+        ss = StockSymbol(api_key)
+        symbol_list_us = ss.get_symbol_list(market="US")  # "us" or "america" will also work
+        for company in symbol_list_us:
+            if company.get('symbol') is not None:
+                companies.append(company.get('symbol'))
+    if save_to_file:
+        with open("ticker_list.txt", "w") as file:
+            for company in companies:
+                file.write(f"{company}\n")
+    return companies
+
+'''
 # Load Data
-company = ['GOOG']  # TODO: do every company
+company = ['NVDA']  # TODO: do every company
 start = dt.datetime(2012, 1, 1)  # Timestamp start, 1st of january
 end = dt.datetime(2020, 1, 1)  # Timestamp end, 1st of january
-#source = 'yahoo'
-#yf.pdr_override()
 data = yf.download(company, start=start, end=end)
-#data = web.get_data_yahoo(company, start=start, end=end).reset_index()
-#data = web.DataReader(company, data_source=source, start=start, end=end).reset_index()
 
 # Prepare Data
 scaler = MinMaxScaler(feature_range=(0, 1))  # scale down all values, so they fit between 0 and 1
@@ -46,7 +92,7 @@ model.add(Dense(units=1))  # predicts next closing value here
 
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, epochs=25, batch_size=32)
-#model.save()
+model.save(os.path.join('models', company[0]))
 #model.load()
 
 # Evaluate the Model Accuracy
@@ -54,7 +100,6 @@ model.fit(x_train, y_train, epochs=25, batch_size=32)
 test_start = dt.datetime(2020, 1, 1)
 test_end = dt.datetime.now()
 test_data = yf.download(company, start=test_start, end=test_end)
-#test_data = web.DataReader(company, 'yahoo', test_start, test_end)
 actual_prices = test_data['Close'].values
 total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
 model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
@@ -79,3 +124,4 @@ plt.xlabel('Time')
 plt.ylabel(f"{company} Share Price")
 plt.legend()
 plt.show()
+'''
